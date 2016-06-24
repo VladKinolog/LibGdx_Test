1c5237d5bc3ce7d9b1b24176840eea6b4885ca45
Vladyslav Koveha <8viper4@gmail.com>
2016-06-24 17:24:39 +0300

    + Шар перемещается по лаберинту при помощи кнопок вверх, вних,влево и вправо.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,27 +2,40 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MyGdxGame extends ApplicationAdapter {
+    Rectangle ball;
+    Rectangle wall;
 	OrthographicCamera camera;
 	SpriteBatch batch;
-	Texture img;
+	Texture wallImg;
+    Texture ballImg;
 	Maze maze;
 	int mazeWidth = 28;
 	int mazeHeight = 28;
+    int xBall = 16;
+    int yBall = 16;
+
 	
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,800,480);
 		batch = new SpriteBatch();
-		img = new Texture("Wall.png");
-		maze = new Maze(mazeWidth,mazeHeight);
+		wallImg = new Texture("Wall.png");
+        ballImg = new Texture("ball.png");
+		maze = new Maze(mazeWidth, mazeHeight);
 		maze.generatorMaze();
+        wall = new Rectangle();
+        wall.x = 0;
+        wall.y = 0;
+
 
 	}
 
@@ -30,14 +43,45 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		for (int i=0; i<=mazeWidth; i++) {
-			for (int y=0; y<=mazeHeight; y++) {
+		for (int i=0; i< maze.getMazeArray().length; i++) {
+			for (int y=0; y< maze.getMazeArray().length; y++) {
 				if (maze.getMazeArray()[i][y] == Maze.WALL)
-				batch.draw(img, i * 16, y * 16);
+				batch.draw(wallImg, i * 16, y * 16);
 			}
 		}
-
+        batch.draw(ballImg,xBall,yBall);
 		batch.end();
+
+//          Обработка нажатия клавиш и столкновение
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+            if (maze.getMazeArray()[(xBall - 16)/16][yBall/16]== Maze.WALL){return;}
+            xBall -= 16;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+            if (maze.getMazeArray()[(xBall + 16)/16][yBall/16]== Maze.WALL){return;}
+            xBall += 16;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+            if (maze.getMazeArray()[xBall/16][(yBall+16)/16]== Maze.WALL){return;}
+            yBall += 16;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+            if (maze.getMazeArray()[xBall/16][(yBall-16)/16]== Maze.WALL){return;}
+            yBall -= 16;
+        }
+//        Проверка выхода за пределы экрана
+        if(xBall <  16) xBall = 16;
+        if(xBall > maze.getMazeArray().length * 16 - 32) xBall = maze.getMazeArray().length * 16 - 32;
+        if(yBall < 16) yBall = 16;
+        if(yBall > maze.getMazeArray().length * 16 - 32) yBall = maze.getMazeArray().length * 16 - 32;
 	}
+
+    @Override
+    public void dispose() {
+        wallImg.dispose();
+        ballImg.dispose();
+    }
 }
